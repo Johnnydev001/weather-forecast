adaa81f95e3547f82c8d2a0efb86b5da7035b67f
João Saraiva <joao.saraiva@diconium.com>
2024-08-26 18:27:40 +0100

    Adding the JsonPropertyName for better mapping

--- backend/Models/Weather/WeatherModel.cs
+++ backend/Models/Weather/WeatherModel.cs
@@ -1,13 +1,21 @@
+using System.Text.Json.Serialization; 
  public class CurrentWeather {
-    public float temp { get; set; }
+    [JsonPropertyName("temperature")]
+
+   public float temp { get; set; }
+
+   [JsonPropertyName("feelsLike")]
     public float feels_like { get; set; }
 
     public float pressure {get; set;}
 
+   [JsonPropertyName("uvIndex")]
     public float uvi { get; set; }
 
+   [JsonPropertyName("cloudsPercentage")]
     public float clouds {get; set;}
 
+[  JsonPropertyName("windSpeed")]
     public float wind_speed {get; set;}
 
     public float humidity {get; set;}
